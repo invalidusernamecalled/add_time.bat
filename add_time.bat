f796ff9b100b24abc48c122ff5ebbb5ab5297f8f
invalidusernamecalled <puneetbapna@gmail.com>
2025-10-05 17:43:11 +0530

    Add files via upload

--- add_time.bat
+++ add_time.bat
@@ -6,28 +6,40 @@ if /i "%~1"=="/add" set op_=+
 
 if "%op_%" NEQ "" goto validate_arg_ok1
 
+echo:ERROR:invalid first argument
+
 goto :eof
 
 :validate_arg_ok1
 
-echo %~2| findstr /v "^[0-9]*$"&&goto :eof
+echo %~2| findstr /v "^[0-9]*$" >NUL&&(echo:ERROR:invalid second argument&goto :eof)
 
 set "add_seconds=%~2"
 set /a add_seconds=add_seconds*60
 
+call :setok & goto validate_arg_ok2
+
+:setok
+
+exit /b 0
+
 :validate_arg_ok2
-
+set time_str=
 for /f "tokens=2 delims=^=" %%i in ('wmic os get localdatetime /value') do set time_str=%%i
-
+if "%time_str%"=="" echo ERROR:failed to get time&goto :eof
 set hours_str=%time_str:~8,2%
 set minutes_str=%time_str:~10,2%
 set seconds_str=%time_str:~12,2%
 
 Set /a total_seconds=0
 set /a total_seconds=(hours_str*60*60)+total_seconds
+if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
 set /a total_seconds=(minutes_str*60)+total_seconds
+if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
 set /a total_seconds=(seconds_str)+total_seconds
+if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
 set /a total_seconds=total_seconds %op_% add_seconds
+if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
 
 set /a result_hours=total_seconds / 3600
 set /a result_hours_modulus=total_seconds %% 3600
