73c5d5650ed379f05a05adccf4c55bbe24c18b4f
invalidusernamecalled <puneetbapna@gmail.com>
2025-10-05 18:44:43 +0530

    Add files via upload

--- add_time.bat
+++ add_time.bat
@@ -4,14 +4,18 @@ set op_=
 if /i "%~1"=="/t"  goto validate_arg_ok1
 if /i "%~1"=="/m" goto validate_arg_ok1
 if /i "%~1"=="/c" goto validate_arg_ok1
+if /i "%~1"=="/p" goto validate_arg_ok1
+
 echo %~1| findstr /v "^[-+][0-9]*$" >NUL&&(echo:ERROR:invalid first argument&goto :checksyntax) || (set "add_seconds=%~1")
 
 :validate_arg_ok1
+
 if "%~2"=="" call :setok & goto validate_arg_ok2
 echo %~2| findstr /v "^[-+][0-9]*$" >NUL&&(echo: >NUL) || (set "add_seconds=%~2"&call :setok & goto validate_arg_ok2)
 if /i "%~2"=="/t"  call :setok & goto validate_arg_ok2
 if /i "%~2"=="/m" call :setok & goto validate_arg_ok2
 if /i "%~2"=="/c" call :setok & goto validate_arg_ok2
+if /i "%~2"=="/p" call :setok & goto validate_arg_ok2
 echo:ERROR:invalid second argument
 goto :eof
 
@@ -24,10 +28,15 @@ echo:     Where {X} is:
 echo:                               /c current time
 echo:                               /t prints total time in seconds
 echo:                               /m prints in format HH:mm
+echo:                               /p convert seconds to time format
 echo:     Where {Number} is:
 echo:                               number of Minutes:-any integer (not too large)
+echo:                               number of Seconds:-for only /p option
 goto :eof
 :validate_arg_ok2
+if /i "%~2"=="/p" set /a "total_seconds=%~1"&goto :calculate_from_total_seconds
+if /i "%~1"=="/p" set /a "total_seconds=%~2"&goto :calculate_from_total_seconds
+
 set /a add_seconds=add_seconds*60
 set time_str=
 
@@ -48,7 +57,7 @@ set /a total_seconds=(seconds_str)+total_seconds
 if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
 set /a total_seconds=total_seconds + add_seconds
 if %errorlevel% NEQ 0 echo ERROR:illegal operation&goto :eof
-
+:calculate_from_total_seconds
 set /a result_hours=total_seconds / 3600
 if %result_hours%==24 set /a result_hours=0
 if %result_hours% GTR 24 set /a result_hours=result_hours %% 24
